5d330052576c6b6befc297ca0c5175d00ac7be7e
pranavraina872 <pranavraina17@gmail.com>
2026-01-30 12:50:44 +0530

    new add

---
+++ src/main/java/com/firstproject/HELLO.java
@@ -0,0 +1,11 @@
+package com.firstproject;
+
+import java.sql.SQLOutput;
+
+public class HELLO {
+    public static void main(String[]args){
+        System.out.println("hellow world");
+    }
+
+
+}
